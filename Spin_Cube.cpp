588dc5b01ad2ab7bb0da94db30b575e9c068278b
Prajwal K B <kbprajwal8@gmail.com>
2022-07-01 00:37:48 +0530

    Update: typed something here.

---
+++ Spin_Cube.cpp
@@ -0,0 +1,11 @@
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+  cout << "This is a test code to check if I can make signed commits for this "
+          "repo."
+       << endl;
+}
